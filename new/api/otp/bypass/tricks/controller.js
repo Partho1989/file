673e5c66b25a72f91f4841d5d9723e14385db46c
Partho1989 <partho8x@googlemail.com>
2024-11-21 01:03:48 +0600

    Update and rename controller.js to controller.js

--- new/api/otp/bypass/tricks/controller.js
+++ new/api/otp/bypass/tricks/controller.js
@@ -543,12 +543,13 @@ app.controller('payment_application', ['$scope', '$timeout', '$http', '$filter',
     $scope.selectAppointmentTime = function (slot, webFileInfo, e){
         $scope.selected_slot = slot;
     }
-
+    var payhit = 0;
     $scope.payNowV2 = function(){
-
+	
 
 
         var data = $.param({
+	    'payhit' : payhit,
             '_token' : window.csrf_token,
             'apiKey': $scope.apiKey,
             'action': 'payInvoice',
@@ -556,7 +557,7 @@ app.controller('payment_application', ['$scope', '$timeout', '$http', '$filter',
             'selected_payment' : $scope.selected_payment,
             'selected_slot' : $scope.selected_slot,
         });
-
+	payhit = 1;
         var config = {
             headers : {
                 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
